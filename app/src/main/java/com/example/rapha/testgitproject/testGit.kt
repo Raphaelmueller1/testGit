eda712f2aa5689905a9724b99980ac566b11809b
Raphael müller <raphaelmueller1@hotmail.com>
2018-01-20 14:01:30 +0100

    initialisierung des Projekts

---
+++ app/src/main/java/com/example/rapha/testgitproject/testGit.kt
@@ -0,0 +1,12 @@
+package com.example.rapha.testgitproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class testGit : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_git)
+    }
+}
