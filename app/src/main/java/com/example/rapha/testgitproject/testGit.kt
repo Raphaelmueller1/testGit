9dbacd47276bb4ac14cd95783eb3129b19949a1a
Rapha <raphaelmueller1@hotmail.com>
2018-01-20 14:35:35 +0100

    added new comment

--- app/src/main/java/com/example/rapha/testgitproject/testGit.kt
+++ app/src/main/java/com/example/rapha/testgitproject/testGit.kt
@@ -9,5 +9,6 @@ class testGit : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_git)
         println("hello World")
+        println("second commit")
     }
 }
