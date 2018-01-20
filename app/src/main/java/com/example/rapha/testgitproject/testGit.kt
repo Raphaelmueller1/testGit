ec47a8e720cb1701659338ba776f266e1334920a
Rapha <raphaelmueller1@hotmail.com>
2018-01-20 14:28:24 +0100

    added hello world comment

--- app/src/main/java/com/example/rapha/testgitproject/testGit.kt
+++ app/src/main/java/com/example/rapha/testgitproject/testGit.kt
@@ -8,5 +8,6 @@ class testGit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_git)
+        println("hello World")
     }
 }
